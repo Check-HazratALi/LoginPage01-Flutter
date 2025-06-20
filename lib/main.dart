a0a6b847ed21f591e8b8d0cb6e569bcdb2d8b7ff
Check-HazratALi <162535361+Check-HazratALi@users.noreply.github.com>
2025-06-20 22:58:37 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:project_1/HomePage/homePage.dart';
 
-void main ()=> runApp(
-  MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Homepage(),
-  )
-);
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Homepage(), // or your home widget
+    );
+  }
+}
